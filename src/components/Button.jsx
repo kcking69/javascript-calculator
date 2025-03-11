7ae2ec0c77fe5b42461dfdd5800fc32bef00c48d
Nkemdilim Collins <collinsnkemdilim05@gmail.com>
2025-03-11 16:45:12 +0100

    first version completed

--- src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-const Button = ({ name, id, handleButton }) => {
-  return (
-    <div className='button' id={id} onClick={handleButton}>
-      {name}
-    </div>
-  );
-};
-
-export default Button;
